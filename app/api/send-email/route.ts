ed20eaf0bd9a282329db28fecd0a9c18f8234f59
Jonmar Montaniel <jonmarmontaniel@gmail.com>
2023-12-28 02:03:15 +0100

    Remove: Email sender

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -2,15 +2,17 @@ import { Resend } from 'resend'
 import WelcomeTemplate from '@/emails/WelcomeTemplate'
 import { NextResponse } from 'next/server'
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+// Commented out because this is just for testing purposes
+
+// const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST() {
-  await resend.emails.send({
-    from: 'jonmar@daytwo.no',
-    to: 'jonmarmontaniel@gmail.com',
-    subject: 'Hello from Resend!',
-    react: WelcomeTemplate({ name: 'Jonmar Montaniel' }),
-  })
+  // await resend.emails.send({
+  //   from: 'jonmar@daytwo.no',
+  //   to: 'jonmarmontaniel@gmail.com',
+  //   subject: 'Hello from Resend!',
+  //   react: WelcomeTemplate({ name: 'Jonmar Montaniel' }),
+  // })
 
   return NextResponse.json({})
 }
